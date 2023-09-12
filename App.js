82be6d889e1463b670d9fb1c2caf8d7a34ad2e10
Shreea Sri <shreeasri@gmail.com>
2023-09-12 17:26:37 +0530

    initializing my food ordering application

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="head" tabIndex="1">
+    Helloo react using JSX
+  </h1>
+);
+
+//comopnent composition
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading"> hey component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
